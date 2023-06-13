df289e4d86373dd7fd0bcdb3bea1495761aae04b
Richard Perez Jr <richard.p23@icloud.com>
2023-06-13 01:04:33 -0400

    Revert "Attempting downgrade to Nextjs 13.2.3 to fix bugs"
    This reverts commit d8e4c4b28d492a465318c7d979b8079dececa698.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  experimental: {
-    appDir: true,
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;

d8e4c4b28d492a465318c7d979b8079dececa698
Richard Perez Jr <richard.p23@icloud.com>
2023-06-13 01:04:33 -0400

    Attempting downgrade to Nextjs 13.2.3 to fix bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
